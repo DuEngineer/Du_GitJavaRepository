a709ca4e59394e57d51cbea298029d936df2870d
DZH <2247677474@qq.com>
2021-05-16 12:23:27 +0800

    DZH 增加Servlet

---
+++ src/main/java/com/du/hd_servlet/LoginServlet.java
@@ -0,0 +1,25 @@
+package com.du.hd_servlet;
+
+
+import com.alibaba.fastjson.JSON;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        user.setUsername(request.getParameter("username"));
+        user.setPassword(request.getParameter("password"));
+
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().write(JSON.toJSONString(user));
+    }
+}
